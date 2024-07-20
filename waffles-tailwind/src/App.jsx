163b27e7312a28f69eeefe85c420921da7520a82
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-20 16:47:55 +0100

    icon processing

--- waffles-tailwind/src/App.jsx
+++ waffles-tailwind/src/App.jsx
@@ -7,15 +7,31 @@ function App() {
     <div className='mx-32 '>
      <NavBar />
 
+            <ul className='mt-6'>
+
+                <div className='flex gap-2 '>
+                   
+                    <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+                      <div>
+                      <li className='font-medium'>Customizable CRM</li>
+                      <p>Centralize your essential data <br /> and systems.</p>
+                      </div>
+                </div>
+            </ul>
+
       <div className='text-center py-24'>
       <h1 className='text-7xl font-semibold tracking-tight ' >The core infrastructure <br /> enabling your operations</h1>
       <p className='py-8 text-gray-700 text-lg' >Effortlessly create custom CRMs, internal tools, customer portals <br />
        and other business applications for your team.  Get started in minutes, no coding necessary.</p>
       </div>
+      <img src={Support} alt="logo for blank" className='w-120 '/>
 
 
-    <img src={Support} alt="logo for blank" className='w-120 '/>
-    </div>
+
+
+
+
+      </div>
   )
 }
 
