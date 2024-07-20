802e26d174b90d89db1ca7803d6025e90900b681
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-20 16:15:59 +0100

    background url add

--- waffles-tailwind/src/App.jsx
+++ waffles-tailwind/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 import NavBar from './Components/NavBar'
+import Support from './assets/Frame 110.png'
 
 function App() {
   return (
-    <div>
+    <div className='mx-32 '>
      <NavBar />
+
+      <div className='text-center py-24'>
+      <h1 className='text-7xl font-semibold tracking-tight ' >The core infrastructure <br /> enabling your operations</h1>
+      <p className='py-8 text-gray-700 text-lg' >Effortlessly create custom CRMs, internal tools, customer portals <br />
+       and other business applications for your team.  Get started in minutes, no coding necessary.</p>
+      </div>
+
+
+    <img src={Support} alt="logo for blank" className='w-120 '/>
     </div>
   )
 }
--- waffles-tailwind/src/Components/NavBar.jsx
+++ waffles-tailwind/src/Components/NavBar.jsx
@@ -5,22 +5,22 @@ const NavBar = () => {
   return (
     <div className='flex items-center justify-between'>
 
-      <img src={Logo} alt="logo for blank" className='w-24 '/>
+      <img src={Logo} alt="logo for blank" className='w-20 '/>
 
       <div>
-        <ul className='flex gap-5'>
-            <li>Product</li>
-            <li>Use Case</li>
-            <li>Integration</li>
-            <li>Resources</li>
-            <li>Pricing</li>
+        <ul className='flex gap-5 text-gray-700 font-normal hover:text-black '>
+            <li className='cursor-pointer'>Product</li>
+            <li className='cursor-pointer'>Use Case</li>
+            <li className='cursor-pointer'>Integration</li>
+            <li className='cursor-pointer'>Resources</li>
+            <li className='cursor-pointer'>Pricing</li>
         </ul>
       </div>
 
 
       <div className='flex items-center gap-5 ' >
         <button>Sign In</button>
-        <button className='bg-black text-white px-6 py-2 rounded-lg'>Try Starter for free</button>
+        <button className='bg-black text-white px-4 py-2 rounded-lg'>Try Starter for free</button>
       </div>
     </div>
   )
