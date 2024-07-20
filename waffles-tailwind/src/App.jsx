6f3cd17ca8646b069f547f76b84b090416f12841
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-20 18:47:51 +0100

    layout set

--- waffles-tailwind/src/App.jsx
+++ waffles-tailwind/src/App.jsx
@@ -7,17 +7,93 @@ function App() {
     <div className='mx-32 '>
      <NavBar />
 
-            <ul className='mt-6'>
 
-                <div className='flex gap-2 '>
-                   
-                    <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+            <div className='border-dropdown flex items-center justify-between  px-9 py-7 rounded-2xl' id='border-dropdown'>
+
+            <div >
+           
+              <div className='flex gap-2 py-4 '>
+                      <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
                       <div>
-                      <li className='font-medium'>Customizable CRM</li>
-                      <p>Centralize your essential data <br /> and systems.</p>
+                      <div className='font-medium'>Customizable CRM</div>
+                      <p className='text-gray-700'>Centralize your essential data <br /> and systems.</p>
                       </div>
                 </div>
-            </ul>
+
+                <div className='flex gap-2 py-4 '>
+                      <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+                      <div>
+                      <div className='font-medium'>Customer Portal</div>
+                      <p className=''>Delight your customers with self <br/> service experience..</p>
+                      </div>
+                </div>
+
+                <div className='flex gap-2 py-4 '>
+                      <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+                      <div>
+                      <div className='font-medium'>Streamlining processes</div>
+                      <p className='text-gray-700'>Unleash the power of organized <br/> knowledge.</p>
+                      </div>
+                </div>
+            </div>
+
+
+            <div>
+              <div className='flex gap-2 py-4 '>
+                      <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+                      <div>
+                      <div className='font-medium'>Customizable CRM</div>
+                      <p className='text-gray-700'>Centralize your essential data <br /> and systems.</p>
+                      </div>
+                </div>
+
+                <div className='flex gap-2 py-4 '>
+                      <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+                      <div>
+                      <div className='font-medium'>Customer Portal</div>
+                      <p className=''>Delight your customers with self <br/> service experience..</p>
+                      </div>
+                </div>
+
+                <div className='flex gap-2 py-4 '>
+                      <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+                      <div>
+                      <div className='font-medium'>Streamlining processes</div>
+                      <p className='text-gray-700'>Unleash the power of organized <br/> knowledge.</p>
+                      </div>
+                </div>
+            </div>
+
+
+            <div>
+              <div className='flex gap-2 py-4 '>
+                      <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+                      <div>
+                      <div className='font-medium'>Customizable CRM</div>
+                      <p className='text-gray-700'>Centralize your essential data <br /> and systems.</p>
+                      </div>
+                </div>
+
+                <div className='flex gap-2 py-4 '>
+                      <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+                      <div>
+                      <div className='font-medium'>Customer Portal</div>
+                      <p className=''>Delight your customers with self <br/> service experience..</p>
+                      </div>
+                </div>
+
+                <div className='flex gap-2 py-4 '>
+                      <div className="div-icons w-12 h-12 rounded-lg bg-black"></div>
+                      <div>
+                      <div className='font-medium'>Streamlining processes</div>
+                      <p className='text-gray-700'>Unleash the power of organized <br/> knowledge.</p>
+                      </div>
+                </div>
+            </div>
+
+            </div>
+
+            
 
       <div className='text-center py-24'>
       <h1 className='text-7xl font-semibold tracking-tight ' >The core infrastructure <br /> enabling your operations</h1>
