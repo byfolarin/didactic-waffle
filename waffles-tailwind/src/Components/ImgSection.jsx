5b87ca1e9678f692b36be5f2cd76568e59ef1d9a
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-23 11:12:06 +0100

    expecting sec

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const ImgSection = () => {
   return (
     <div>
-     <div className='w-full h-36 my-11 relative bg-slate-300 '></div> 
+     <div className='w-full  h-36 my-11 relative bg-slate-400 '>
+      
+      <div className=''></div>
+      </div> 
     </div>
   )
 }
