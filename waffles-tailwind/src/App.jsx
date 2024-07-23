97555c23035895a0ed95d06cda91348bbcf6d5d9
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-23 11:02:10 +0100

    Img Section

--- waffles-tailwind/src/App.jsx
+++ waffles-tailwind/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import NavBar from './Components/NavBar'
 import Mesh from './assets/Mesh.png'
 import H1comp from './Components/H1comp'
+import ImgSection from './Components/ImgSection'
 
 
 
@@ -12,13 +13,10 @@ function App() {
     <div className="flex  w-full " >
     <img src={Mesh} className='absolute top-0 bottom-0 left-0 right-0' alt="" />
     </div>
-  
-    
-
     <NavBar />
-      
     <H1comp />
-      </div>
+    <ImgSection/>
+    </div>
   
            
     
---
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImgSection = () => {
+  return (
+    <>
+     <div className='w-full h-11 m-11 bg-slate-300 '></div> 
+    </>
+  )
+}
+
+export default ImgSection
