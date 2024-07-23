4ba0bef7f69bc65d68d2516b84ca97f9d6576796
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-23 11:18:36 +0100

    img container

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const ImgSection = () => {
   return (
     <div>
-     <div flex className='w-full  h-36 my-11 relative bg-slate-400 '>
-          <div className='w-11 bg-slate-500'></div>
-          <div className='w-11 bg-slate-500'></div>
-          <div className='w-11 bg-slate-500'></div>
+     <div className='w-full flex gap-3 justify-between  h-36 my-11 relative bg-slate-400 '>
+          <div className='w-11 h-36  bg-slate-500'></div>
+          <div className='w-11 h-36  bg-slate-500'></div>
+          <div className='w-11 h-36  bg-slate-500'></div>
     </div> 
     </div>
   )
