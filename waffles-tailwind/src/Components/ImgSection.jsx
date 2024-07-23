bd4b0855f78d5a927c3eb4b2284d91697f9673f2
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-23 11:33:07 +0100

    commit in

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import dash1 from '../assets/Frame 828.png'
 
 const ImgSection = () => {
   return (
     <div>
-     <div className='w-full flex gap-3 justify-between  h-36 my-11 relative bg-slate-400 '>
-          <div className='w-11 h-36  bg-slate-500'></div>
-          <div className='w-11 h-36  bg-slate-500'></div>
-          <div className='w-11 h-36  bg-slate-500'></div>
+     <div className='w-full flex gap-3 justify-between  h-96 my-11 relative'>
+          <div className='w-1/3 h-96  bg-gray-200' >
+             <img className='flex align-bottom justify-start' src={dash1} alt="" />
+          </div>
+          <div className='w-1/3 h-96  bg-gray-200'></div>
+          <div className='w-1/3 h-96  bg-gray-200'></div>
     </div> 
     </div>
   )
