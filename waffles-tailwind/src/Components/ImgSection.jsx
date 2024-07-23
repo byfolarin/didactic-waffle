7f0888d53663647594d9d64bc8757000d497ca3b
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-23 11:14:30 +0100

    sec

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const ImgSection = () => {
   return (
     <div>
-     <div className='w-full  h-36 my-11 relative bg-slate-400 '>
-      
-      <div className=''></div>
-      </div> 
+     <div flex className='w-full  h-36 my-11 relative bg-slate-400 '>
+          <div className='w-11 bg-slate-500'></div>
+          <div className='w-11 bg-slate-500'></div>
+          <div className='w-11 bg-slate-500'></div>
+    </div> 
     </div>
   )
 }
