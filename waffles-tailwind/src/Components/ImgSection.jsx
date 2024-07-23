f39215d1a0e036cd465d8fa8e5d40343d5d23353
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-23 11:41:29 +0100

    img selection

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import dash1 from '../assets/Frame 828.png'
+import dash2 from '../assets/image.png'
+import dash3 from '../assets/Frame 6.png'
+
 
 const ImgSection = () => {
   return (
     <div>
      <div className='w-full flex gap-3 justify-between  h-96 my-11 relative'>
           <div className='w-1/3 h-96  bg-gray-200' >
-             <img className='flex align-bottom justify-start' src={dash1} alt="" />
+             <img className='' src={dash1} alt="" />
           </div>
-          <div className='w-1/3 h-96  bg-gray-200'></div>
-          <div className='w-1/3 h-96  bg-gray-200'></div>
+          <div className='w-1/3 h-96  bg-gray-200'>
+          <img src={dash2} alt="" />
+          </div>
+          <div className='w-1/3 h-96  bg-gray-200'>
+          <img src={dash3} alt="" />
+          </div>
     </div> 
     </div>
   )
