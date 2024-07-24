4245b208dcb31aace3f793146196d1c397f9080a
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-24 09:26:53 +0100

    changed

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -14,7 +14,16 @@ const ImgSection = () => {
           </div>
 
           <div className='card second w-1/3 bg-gray-200'>
-             <img src={dash2} alt="" class="object-contain" />
+
+                <div  className='bg-gray-600 flex absolute align-middle justify-center px-2'>
+                  <div className='bg-black w-12 h-12'></div>
+                    <div className='flex flex-col align-bottom justify-center py-6 p-2'>
+                        <p>Shipping Order all in a single CRM</p>
+                        <p>Centralize your essential data and systems.</p>
+                    </div>
+                </div>
+                 <img src={dash2} alt=""  class="object-contain w-128" />
+
           </div>
 
           <div className='card third w-1/3  bg-gray-200'>
