1263658498bcbfd12970853bfc0e5f768f428e96
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-24 09:40:36 +0100

    sec

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -9,24 +9,32 @@ const ImgSection = () => {
     <div>
      <div className='w-full flex gap-3 justify-between  h-96 my-11 relative'>
 
-          <div className='card first w-1/3  bg-gray-200' >
+          <div className='card first   bg-gray-200' >
              <img  src={dash1} alt="" />
           </div>
 
-          <div className='card second w-1/3 bg-gray-200'>
+          <div className='card second relative bg-gray-200'>
 
-                <div  className='bg-gray-600 flex absolute align-middle justify-center px-2'>
-                  <div className='bg-black w-12 h-12'></div>
-                    <div className='flex flex-col align-bottom justify-center py-6 p-2'>
-                        <p>Shipping Order all in a single CRM</p>
-                        <p>Centralize your essential data and systems.</p>
+              <div className='p-2 '>
+
+                    <div  className='set-card bg-white flex absolute align-middle bottom-0 justify-center px-2'>
+
+                      <div className='bg-black w-12 h-12'></div>
+
+                        <div className='flex flex-col align-bottom justify-center py-6 p-2'>
+                            <p>Shipping Order all in a single CRM</p>
+                            <p>Centralize your essential data and systems.</p>
+                        </div>
+
                     </div>
+
                 </div>
+
                  <img src={dash2} alt=""  class="object-contain w-128" />
 
           </div>
 
-          <div className='card third w-1/3  bg-gray-200'>
+          <div className='card third  bg-gray-200'>
              <img src={dash3} alt="" />
           </div>
 
