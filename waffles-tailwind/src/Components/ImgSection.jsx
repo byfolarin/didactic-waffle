55168c2a4d003638993574604cc36854dbde90b2
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-24 14:18:39 +0100

    explorations

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import dash1 from '../assets/Frame 828.png'
 import dash2 from '../assets/image.png'
 import dash3 from '../assets/Frame 6.png'
+import Stack from '../assets/Stacks.png'
 
 
 const ImgSection = () => {
@@ -10,7 +11,7 @@ const ImgSection = () => {
      <div className='w-full flex gap-3 justify-between  h-96 my-11 relative'>
 
           <div className='card first rounded-lg  bg-gray-200' >
-             <img  src={dash1} alt="" />
+             <img  src={dash1} alt=""/>
           </div>
 
           <div className='card second relative rounded-lg bg-gray-200 w-full h-full overflow-hidden'>
@@ -18,13 +19,19 @@ const ImgSection = () => {
           <img src={dash2} alt="" className="object-cover w-full h-full" />
 
           <div className='absolute inset-0 p-2 flex items-end'>
+
             <div className='set-card rounded-lg bg-white flex items-center w-full px-5'>
-              <div className='bg-black rounded-lg size-12 flex-shrink-0'></div>
+              <div className='bg-black rounded-lg size-12 flex-shrink-0'>
+                <img src={Stack} alt="" />
+              </div>
+
               <div className='flex flex-col justify-center py-6 p-2 flex-grow'>
                 <p className='text-sm font-semibold'>Shipping Order all in a single CRM</p>
                 <p className='font-sans text-xs'>Centralize your essential data and systems.</p>
               </div>
+
             </div>
+
           </div>
 
           </div>
