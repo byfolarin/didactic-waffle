481aafe0576f19924bb005f7e2a0b23e90dd33b3
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-24 08:37:32 +0100

    excel-w

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -9,7 +9,7 @@ const ImgSection = () => {
     <div>
      <div className='w-full flex gap-3 justify-between  h-96 my-11 relative'>
 
-          <div className='w-1/3 h-96  bg-gray-200' >
+          <div className='w-1/3  bg-gray-200' >
              <img className='' src={dash1} alt="" />
           </div>
 
@@ -17,7 +17,7 @@ const ImgSection = () => {
              <img className='flex align-middle justify-center' src={dash2} alt="" />
           </div>
 
-          <div className='w-1/3 h-96  bg-gray-200'>
+          <div className='w-1/3 bg-gray-200'>
              <img className='w-42' src={dash3} alt="" />
           </div>
 
