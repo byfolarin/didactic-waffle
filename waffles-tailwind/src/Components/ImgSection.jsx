ac76152fdf5ada43abc6e6f8355ee8ae636a8e06
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-24 10:23:08 +0100

    rounded

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -9,27 +9,27 @@ const ImgSection = () => {
     <div>
      <div className='w-full flex gap-3 justify-between  h-96 my-11 relative'>
 
-          <div className='card first   bg-gray-200' >
+          <div className='card first rounded-lg  bg-gray-200' >
              <img  src={dash1} alt="" />
           </div>
 
-          <div className='card second relative bg-gray-200 w-full h-full overflow-hidden'>
+          <div className='card second relative rounded-lg bg-gray-200 w-full h-full overflow-hidden'>
 
-<img src={dash2} alt="" className="object-cover w-full h-full" />
+          <img src={dash2} alt="" className="object-cover w-full h-full" />
 
-<div className='absolute inset-0 p-2 flex items-end'>
-  <div className='set-card rounded-lg bg-white flex items-center w-full'>
-    <div className='bg-black size-12 flex-shrink-0'></div>
-    <div className='flex flex-col justify-center py-6 p-2 flex-grow'>
-      <p className='text-sm font-semibold'>Shipping Order all in a single CRM</p>
-      <p className='font-sans text-xs'>Centralize your essential data and systems.</p>
-    </div>
-  </div>
-</div>
+          <div className='absolute inset-0 p-2 flex items-end'>
+            <div className='set-card rounded-lg bg-white flex items-center w-full px-5'>
+              <div className='bg-black rounded-lg size-12 flex-shrink-0'></div>
+              <div className='flex flex-col justify-center py-6 p-2 flex-grow'>
+                <p className='text-sm font-semibold'>Shipping Order all in a single CRM</p>
+                <p className='font-sans text-xs'>Centralize your essential data and systems.</p>
+              </div>
+            </div>
+          </div>
 
-</div>
+          </div>
 
-          <div className='card third  bg-gray-200'>
+          <div className='card third  rounded-lg bg-gray-200'>
              <img src={dash3} alt="" />
           </div>
 
