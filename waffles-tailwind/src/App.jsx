33b424ed425db0245e1e0bd1defc4258f20151ee
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-25 12:15:43 +0100

    mag struc

--- waffles-tailwind/src/App.jsx
+++ waffles-tailwind/src/App.jsx
@@ -5,6 +5,7 @@ import H1comp from './Components/H1comp'
 import ImgSection from './Components/ImgSection'
 import FirstSection from './Components/FirstSection'
 import SecondSection from './Components/SecondSection'
+import one from './assets/Frame 87.png'
 
 
 
@@ -20,6 +21,48 @@ function App() {
     <ImgSection/>
     <FirstSection />
     <SecondSection/>
+
+    <div className='flex my-32 gap-4 '>
+
+
+    
+    <div>
+      <img src={one} className='size-16' alt="" />
+      <div className='mt-2'>
+      <h2 className='font-semibold text-lg'>Build momentum </h2>
+      <p>Create healthy routines and focus your team on what work should happen next.</p>
+      </div>
+    </div>
+
+    <div>
+    <img src={one} className='size-16' alt="" />
+    <div className='mt-2'>
+      <h2 className='font-semibold text-lg'>Manage incoming works</h2>
+      <p>Create healthy routines and focus your team on what work should happen next.</p>
+      </div>
+    </div>
+
+    <div>
+    <img src={one} className='size-16' alt="" />
+    <div className='mt-2'>
+      <h2 className='font-semibold text-lg'>Tailored workflows</h2>
+      <p>Create healthy routines and focus your team on what work should happen next.</p>
+      </div>
+    </div>
+
+    <div>
+      <img src={one} className='size-16' alt="" />
+      <div className='mt-2'>
+      <h2 className='font-semibold text-lg'>Custom views</h2>
+      <p>Create healthy routines and focus your team on what work should happen next.</p>
+      </div>
+    </div>
+
+    </div>
+
+
+
+
     </div>
 
   
