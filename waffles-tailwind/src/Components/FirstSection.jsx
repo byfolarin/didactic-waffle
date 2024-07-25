c7ec4c404f716b773d6bbea6fddd03b1a4da4cfa
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-25 11:47:11 +0100

    made for structure

--- waffles-tailwind/src/Components/FirstSection.jsx
+++ waffles-tailwind/src/Components/FirstSection.jsx
@@ -8,6 +8,12 @@ const FirstSection = () => {
 
 
                 <div className='mt-24'>
+
+                    <div className='flex gap-3 items-center'>
+                    <div className='w-6 h-2 rounded bg-black'></div>
+                    <p>Project and long term planning</p>
+                    </div>
+
                 <h2 className='font-sans font-semibold text-5xl w-3/5 py-6'>Made for Infrastructure<br/>Development</h2>
                 <p className='w-2/4'>Align your team around a unified product timeline. Plan, manage, and track all product initiatives with Linear’s visual planning tools</p>
                 </div>
