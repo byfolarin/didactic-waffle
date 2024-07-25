f11ff6acfe048cf5014c5dc47796487aa5ba38e1
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-25 13:41:47 +0100

    third-section

--- waffles-tailwind/src/App.jsx
+++ waffles-tailwind/src/App.jsx
@@ -5,7 +5,8 @@ import H1comp from './Components/H1comp'
 import ImgSection from './Components/ImgSection'
 import FirstSection from './Components/FirstSection'
 import SecondSection from './Components/SecondSection'
-import one from './assets/Frame 87.png'
+import ThirdSection from './Components/ThirdSection'
+
 
 
 
@@ -21,45 +22,9 @@ function App() {
     <ImgSection/>
     <FirstSection />
     <SecondSection/>
-
-    <div className='flex my-32 gap-4 '>
-
-    <div className='w-1/4'>
-    <img src={one} className='size-10' alt="" />
-    <div className='mt-2'>
-      <h2 className='font-semibold text-lg'>Manage incoming works</h2>
-      <p>Create healthy routines and focus your team on what work should happen next.</p>
-      </div>
-    </div>
-
+    <ThirdSection />
 
     
-    <div className='w-1/4'>
-      <img src={one} className='size-10' alt="" />
-      <div className='mt-2'>
-      <h2 className='font-semibold text-lg'>Build momentum </h2>
-      <p>Track progress across custom issue flows for your team.</p>
-      </div>
-    </div>
-
-
-    <div className='w-1/4'>
-    <img src={one} className='size-10' alt="" />
-    <div className='mt-2'>
-      <h2 className='font-semibold text-lg'>Tailored workflows</h2>
-      <p>Review and assign incoming bug reports, feature requests, and other unplanned work.</p>
-      </div>
-    </div>
-
-    <div className='w-1/4'>
-      <img src={one} className='size-10' alt="" />
-      <div className='mt-2'>
-      <h2 className='font-semibold text-lg'>Custom views</h2>
-      <p>Take the guesswork out of product planning with realtime, actionable data analytics.</p>
-      </div>
-    </div>
-
-    </div>
 
 
 
