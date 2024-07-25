58951493ae9f534e7e8216e74c6ce2d983750d5f
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-25 18:44:55 +0100

    sec h-64

--- waffles-tailwind/src/App.jsx
+++ waffles-tailwind/src/App.jsx
@@ -33,14 +33,14 @@ function App() {
             <h2 className='font-semibold'>Build momentum with Cycles</h2>
             <p>Create healthy routines and focus your team on what work should happen next.</p>
 
-            <div className='bg-slate-200 pt-4 w-full h-24'></div>
+            <div className='bg-slate-200 mt-8 w-full h-64'></div>
         </div>
 
         <div className='w-full h-full border  border-gray-200 py-4 px-6'>
             <h2 className='font-semibold'>Build momentum with Cycles</h2>
             <p>Create healthy routines and focus your team on what work should happen next.</p>
 
-            <div className='bg-slate-200 pt-4 w-full h-24'></div>
+            <div className='bg-slate-200 mt-8 w-full h-64'></div>
         </div>
 
     </div>
