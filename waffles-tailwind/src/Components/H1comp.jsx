aa0c02e443695b677ac8e28e613b2cef3d6ec8b8
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-27 09:17:37 +0100

    push

--- waffles-tailwind/src/Components/H1comp.jsx
+++ waffles-tailwind/src/Components/H1comp.jsx
@@ -4,8 +4,8 @@ import Support from '../assets/Frame 110.png'
 const H1comp = () => {
   return (
     <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
-    <div className='text-left py-12 sm:py-16 md:py-20 lg:py-24 relative'>
-      <h1 className='text-5xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold tracking-tight leading-tight'>
+    <div className='text-left py-12 sm:py-16 md:py-20 lg:py-24 lg:text-center relative'>
+      <h1 className='text-5xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold tracking-tight'>
         The core infrastructure <br className='hidden sm:inline' /> enabling your operations
       </h1>
       <p className='py-4 sm:py-6 md:py-8 text-gray-700 text-base sm:text-lg max-w-3xl mx-auto'>
