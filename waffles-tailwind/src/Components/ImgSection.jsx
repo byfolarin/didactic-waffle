3810c2cff6de3ce38788455b30365c0ba2a5d847
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-27 12:07:41 +0100

    py/px

--- waffles-tailwind/src/Components/ImgSection.jsx
+++ waffles-tailwind/src/Components/ImgSection.jsx
@@ -23,12 +23,12 @@ const ImgSection = () => {
 
                     <div className='absolute inset-0 p-2 flex items-end'>
 
-                          <div className='set-card rounded-lg bg-white flex items-center w-full px-5'>
+                          <div className='set-card rounded-lg bg-white flex items-center w-full px-2'>
                             <div className='bg-black rounded-lg size-12 flex-shrink-0'>
                               <img className='size-9 my-1 mx-auto'  src={Stack} alt="" />
                             </div >
 
-                            <div className='flex flex-col justify-center py-6 p-2 flex-grow'>
+                            <div className='flex flex-col justify-center py-4 p-2 flex-grow'>
                               <p className='text-sm font-semibold'>Shipping Order all in a single CRM</p>
                               <p className='font-sans text-xs'>Centralize your essential data and systems.</p>
                             </div>
