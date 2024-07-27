b2b9f5a50396894077e88e9730f45f6b7f4f1869
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-28 00:14:33 +0100

    hover

--- waffles-tailwind/src/Components/NavBar.jsx
+++ waffles-tailwind/src/Components/NavBar.jsx
@@ -3,33 +3,35 @@ import Logo from '../assets/Blank.png'
 import Dropdown from './Dropdown'
 
 const NavBar = () => {
-
   const [isHovered, setIsHovered] = useState(false);
 
-
-  const onMouseEnter= ()=>{() => setIsHovered(true)}
-  const onMouseLeave= ()=>{() => setIsHovered(false)}
-
-
-
-
   return (
-    <div className='hidden sm:hidden md:hidden lg:flex  items-center justify-between relative'>
-
+    <div className='hidden sm:hidden md:hidden lg:flex items-center justify-between relative'>
       <img src={Logo} alt="logo for blank" className='w-20 '/>
 
       <div>
         <ul className='flex gap-5 text-gray-700 font-normal'>
-            <li className='cursor-pointer  text-gray-700 font-normal hover:text-black '>Product</li>
-            <li className='cursor-pointer  text-gray-700 font-normal hover:text-black '>Use Case <div></div></li>
-            <li className='cursor-pointer  text-gray-700 font-normal hover:text-black '>Integration</li>
-            <li className='cursor-pointer  text-gray-700 font-normal hover:text-black '>Resources</li>
-            <li className='cursor-pointer  text-gray-700 font-normal hover:text-black '>Pricing</li>
+          <li className='cursor-pointer text-gray-700 font-normal hover:text-black'>Product</li>
+          
+          <li 
+            className='cursor-pointer text-gray-700 font-normal hover:text-black relative'
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            Use Case
+            {isHovered && (
+              <div className="absolute top-full left-0 mt-2 w-screen">
+                <Dropdown />
+              </div>
+            )}
+          </li>
+          <li className='cursor-pointer text-gray-700 font-normal hover:text-black'>Integration</li>
+          <li className='cursor-pointer text-gray-700 font-normal hover:text-black'>Resources</li>
+          <li className='cursor-pointer text-gray-700 font-normal hover:text-black'>Pricing</li>
         </ul>
       </div>
 
-
-      <div className='flex items-center gap-5 ' >
+      <div className='flex items-center gap-5'>
         <button>Sign In</button>
         <button className='bg-black text-white px-4 py-2 rounded-lg'>Try Starter for free</button>
       </div>
