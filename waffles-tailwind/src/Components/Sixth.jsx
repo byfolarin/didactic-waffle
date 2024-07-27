fc370959e29dfe0c563c9b524b60113909da519e
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-27 21:16:36 +0100

    laye

--- waffles-tailwind/src/Components/Sixth.jsx
+++ waffles-tailwind/src/Components/Sixth.jsx
@@ -6,12 +6,18 @@ const Sixth = () => {
            <div className='mt-24 text-center justify-center items-center'>
 
             <div className='flex gap-3 justify-center  items-center'>
-            <div className='w-6 h-2 rounded bg-black'></div>
-            <p>CONTACT</p>
+                  <div className='w-6 h-2 rounded bg-black'></div>
             </div>
 
+          
+
             <h2 className='font-sans font-semibold text-center text-5xl py-6 '>Works where your team already is</h2>
-            <p className='text-center justify-center items-center'>Align your team around a unified product timeline. Plan, manage, and track all product initiatives with Blank’s visual planning tools</p>
+
+            <p className='w-3/4 flex text-center justify-center items-center'>Align your team around a unified product timeline. Plan, manage, and track all product initiatives with Blank’s visual planning tools
+            </p>
+
+           
+
             </div>
 
             <button className='py-2 px-6 my-6 rounded-lg text-white items-center justify-center bg-black'>Request a trial</button>
