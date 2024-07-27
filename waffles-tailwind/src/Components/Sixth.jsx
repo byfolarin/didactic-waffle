6bef9685d4c319ec663fb620db4582617703b232
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-27 21:06:23 +0100

    sixth

--- waffles-tailwind/src/Components/Sixth.jsx
+++ waffles-tailwind/src/Components/Sixth.jsx
@@ -3,17 +3,18 @@ import React from 'react'
 const Sixth = () => {
   return (
     <div>
-           <div className='mt-24'>
+           <div className='mt-24 text-center justify-center items-center'>
 
             <div className='flex gap-3 items-center'>
             <div className='w-6 h-2 rounded bg-black'></div>
             <p>Pricing</p>
             </div>
 
-            <h2 className='font-sans font-semibold text-5xl py-6'>Works where your team already is</h2>
+            <h2 className='font-sans font-semibold text-5xl py-6 '>Works where your team already is</h2>
             <p className='w-3/4'>Align your team around a unified product timeline. Plan, manage, and track all product initiatives with Blank’s visual planning tools</p>
             </div>
-       <button className='py-2 px-6 my-6 rounded-lg text-white bg-black'>Request a trial</button>
+
+            <button className='py-2 px-6 my-6 rounded-lg text-white items-center justify-center bg-black'>Request a trial</button>
     </div>
   )
 }
