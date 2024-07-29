504e590de72bc07f9c2fe23d4da1f9bcebb691f0
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-29 13:33:06 +0100

    push

--- waffles-tailwind/src/Components/MenuIcon.jsx
+++ waffles-tailwind/src/Components/MenuIcon.jsx
@@ -1,25 +1,26 @@
 import React, { useState } from 'react'
+import NavBar from './NavBar';
 
 const MenuIcon = () => {
   const [display, setDisplay] = useState(false);
 
-  const handleClick = (e) =>{
-      e.target.value()
-      setDisplay(true)
-  }
 
   return (
-    <div className='lg:hidden'>
+    <div onClick={() =>{setDisplay(true)}}  >
+    
+    <div className= 'lg:hidden' >
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 7H21" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"/>
             <path d="M3 12H21" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"/>
             <path d="M3 17H21" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"/>
     </svg>
+    </div>
 
-
-    {display && <NavMenu />}
+    <NavBar display={display} setDisplay={setDisplay}/>
 
     </div>
+
+
 
 
 
--- waffles-tailwind/src/Components/NavBar.jsx
+++ waffles-tailwind/src/Components/NavBar.jsx
@@ -3,14 +3,14 @@ import Logo from '../assets/Blank.png'
 import Dropdown from './Dropdown'
 import MenuIcon from './MenuIcon';
 
-const NavBar = () => {
+const NavBar = ({display}) => {
 
   const [isHovered, setIsHovered] = useState(false);
  
 
-
   return (
-    <div  className='relative sm:hidden md:hidden lg:block' >
+    <div  className={`${display ? 'hidden' : 'flex'} relative sm:hidden md:hidden lg:block`} >
+
       <div className='flex flex-col  px-4 py-3 lg:p-0  bg-[#f4f4f4] border border-[#ffffff] lg:border-none rounded-lg lg:flex lg:flex-row lg:items-center lg:justify-between'>
         
         <img src={Logo} alt="logo for blank" className='w-20 hidden lg:block'/>
@@ -46,14 +46,9 @@ const NavBar = () => {
       )}
 
 
-      {isHovered && (
-        <div className="absolute left-0 right-0 mt-2 flex justify-center" onClick={() => setIsHovered(true)}
->
-          <div className="w-full z-10 max-w-7xl">
-            <MenuIcon />
-          </div>
-        </div>
-      )}
+     
+      <MenuIcon />
+       
     </div>
   )
 }
