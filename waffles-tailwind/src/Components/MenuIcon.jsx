713140a064f1dcc2b6b826612bb071e6ec58fd02
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-29 11:50:50 +0100

    z-index

--- waffles-tailwind/src/Components/MenuIcon.jsx
+++ waffles-tailwind/src/Components/MenuIcon.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MenuIcon = () => {
+
+  const [display, setDisplay] = useState(false)
+
+
+
   return (
     <div className='lg:hidden'>
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
