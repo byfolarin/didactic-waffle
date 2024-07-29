c3f380ed2856193e9015dc91675895417786168a
Folarin Folarin <thefolafolarin@gmail.com>
2024-07-29 12:31:42 +0100

    click

--- waffles-tailwind/src/Components/MenuIcon.jsx
+++ waffles-tailwind/src/Components/MenuIcon.jsx
@@ -3,6 +3,11 @@ import React, { useState } from 'react'
 const MenuIcon = () => {
   const [display, setDisplay] = useState(false);
 
+  const handleClick = (e) =>{
+      e.target.value()
+      setDisplay(true)
+  }
+
   return (
     <div className='lg:hidden'>
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
