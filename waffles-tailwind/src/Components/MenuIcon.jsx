fd3a52bc5e75e9be02ce5d332cbd6c355437925c
Folarin Folarin <thefolafolarin@gmail.com>
2024-08-04 04:56:54 +0100

    mouseEnter

--- waffles-tailwind/src/Components/MenuIcon.jsx
+++ waffles-tailwind/src/Components/MenuIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MenuIcon = ({ openNav }) => {
   return (
-    <div className='lg:hidden' onClick={openNav}>
+    <div className='lg:hidden' onMouseEnter={openNav}>
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M3 7H21" stroke="#292D32" strokeWidth="1.5" strokeLinecap="round"/>
         <path d="M3 12H21" stroke="#292D32" strokeWidth="1.5" strokeLinecap="round"/>
